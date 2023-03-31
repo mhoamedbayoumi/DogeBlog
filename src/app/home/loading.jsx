f1d71d43c1006b65886208dc999233a4561f87d5
mohamed bayoumi <68474916+mhoamedbayoumi@users.noreply.github.com>
2023-03-31 04:53:15 +0200

    Delete src/app directory

--- src/app/home/loading.jsx
+++
@@ -1,8 +0,0 @@
-function Loading() {
-    return ( 
-    <div>
-        <h1 className="text-center">loading....</h1>
-    </div> );
-}
-
-export default Loading;
--- src/app/usercontext.jsx
+++
@@ -1,2 +0,0 @@
-import { createContext } from "react";
-export const usercontext=createContext(null);
